3553d3ba4e1604954746f796a3bd3310bf819a95
xieisabug <136419808@qq.com>
2024-10-20 22:57:44 +0800

    init

---
+++ src/main.tsx
@@ -0,0 +1,57 @@
+import './index.css'
+import fs from 'fs'
+
+export default class SamplePlugin implements TeaPlugin, TeaAssistantTypePlugin {
+	prompt: string = `
+	按照以下格式进行代码生成：
+	@f-start:/path/to/data
+	your generate code here
+	@f-end
+	
+	格式额外要求：
+	- 生成的path包含对应的文件名
+	- 生成的文件内容需要包含文件的全部内容，不要省略
+
+	以下是对生成代码的要求：
+	`
+
+	config(): Config {
+		return {
+			name: '代码生成',
+			type: ['assistantType']
+		}
+	}
+
+	onPluginLoad(_: SystemApi) {
+		console.log('SamplePlugin init')
+	}
+
+	onInit(assistantTypeApi: AssistantTypeApi): void {
+		assistantTypeApi.typeRegist(1, "代码生成助手");
+		assistantTypeApi.changeFieldLabel('prompt', "遵循要求");
+		assistantTypeApi.addFieldTips('prompt', "内置生成文件内容格式和自动创建文件的命令，请勿修改模型输出遵循的格式，请指定生产");
+		assistantTypeApi.addField('fileScanDirectory', '文件扫描目录', 'string', {
+			required: true
+		});
+		assistantTypeApi.addField('confirmBeforeGenerate', '生成前确认', 'boolean', {
+			required: true
+		});
+
+		assistantTypeApi.runLogic((assistantRunApi: AssistantRunApi) => {
+			assistantRunApi.setAiResponse('@tips-loading:正在生成对应文件');
+			const response: any = assistantRunApi.askAI(assistantRunApi.getUserInput(), assistantRunApi.getModelId(), this.prompt + assistantRunApi.getField('prompt'));
+			let answer:string = response.answer;
+			// 提取多段f-start和f-end之间的内容
+			const fileContents = answer.match(/@f-start:([\s\S]*?)@f-end/g);
+			if (fileContents) {
+				for (const fileContent of fileContents) {
+					const [filePath, content] = fileContent.split(':').map((str: string) => str.trim());
+					fs.writeFileSync(filePath, content);
+				}
+			}
+			assistantRunApi.setAiResponse('@tips-success:生成完成');
+		});
+
+
+	}
+}
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import replace from '@rollup/plugin-replace'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		react(),
+		replace({
+			'process.env.NODE_ENV': JSON.stringify('production'),
+			preventAssignment: true
+		})
+	],
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/main.tsx'),
+			name: 'SamplePlugin',
+			fileName: 'main',
+			formats: ['umd']
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM'
+				},
+				entryFileNames: 'main.js',
+				format: 'umd'
+			},
+		},
+	},
+})
